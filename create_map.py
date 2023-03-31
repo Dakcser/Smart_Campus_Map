9fb9c50f1b30c2bb7d765119286f6870d2a2ce0d
Emil <50385519+Dakcser@users.noreply.github.com>
2023-03-31 23:00:08 +0300

    update create_map.py

--- create_map.py
+++ create_map.py
@@ -2,7 +2,16 @@ import folium
 
 m = folium.Map([65.06, 25.467], zoom_start=14.5)
 
-m.save(outfile="Smart_campus_map.html")
+folium.raster_layers.ImageOverlay(
+    image="./university_map.png",
+    name="university map",
+    bounds=[[65.062044, 25.462502], [65.056337, 25.471510]],
+    opacity=1,
+    interactive=False,
+    cross_origin=False,
+    zindex=1).add_to(m)
+
+m.save(outfile="index.html")
 
 # Display the map
 m 
